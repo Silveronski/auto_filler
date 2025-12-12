db0c99eb93b3b542e833fb7840ccb901c8bd4479
Silveronski <153466946+Silveronski@users.noreply.github.com>
2025-12-12 21:59:07 +0200

    e

--- build/js/clearForm.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll("input").forEach((input) => {
-  input.value = "";
-  input.dispatchEvent(new Event("input", { bubbles: true }));
-  input.dispatchEvent(new Event("change", { bubbles: true }));
-});
